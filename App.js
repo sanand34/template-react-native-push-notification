e1f71f5cc436ccccade57c86099a12e367382838
sanand34 <sanchitanand34@gmail.com>
2021-04-27 00:48:30 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,161 @@
+import React from 'react';
+import { Alert, Button, Linking, Text, View } from 'react-native';
+
+import { PushNotificationAndroid } from './PushNotification';
+
+export default class App extends React.Component {
+
+  /**
+   * On component init
+   */
+  async componentDidMount() {
+    const initialUrl = await Linking.getInitialURL();
+    console.log('initial URL: ', initialUrl);
+    Linking.addEventListener('url', this.handleDeepLink);
+  }
+
+  /**
+   * On component remove
+   */
+  componentWillUnmount() {
+    Linking.removeEventListener('url', this.handleDeepLink);
+  };
+
+  /**
+   * On deep link appear
+   */
+  handleDeepLink = (event) => {
+    console.log('on linking URL');
+    console.log(event);
+  }
+
+  /**
+   * Creates a new notification channel
+   */
+  onCreateChannelPress = () => {
+    try {
+      const channelId = "my_channel_id";
+      const channelName = "my_channel_name";
+      const channelDesc = "my_channel_desc";
+      const channelImportance = PushNotificationAndroid.CHANNEL_IMPORTANCE_DEFAULT;
+      PushNotificationAndroid.createChannel(channelId, channelName, channelDesc, channelImportance);
+      Alert.alert("Success", `Channel ${channelName} created`);
+    } catch (err) {
+      Alert.alert("Error", err);
+    }
+  };
+
+  /**
+   * Show device FCM token
+   */
+  onGetDeviceTokenPress = async () => {
+    try {
+      const token = await PushNotificationAndroid.getDeviceToken();
+      Alert.alert("Token", token);
+      console.log(token);
+    } catch(err) {
+      Alert.alert("Error", err);
+    }
+  };
+
+  /**
+   * Show standard notification
+   */
+  onShowCommonNotificationPress = () => {
+    try {
+      const notificationId = 1;
+      const template = PushNotificationAndroid.TEMPLATE_COMMON;
+      const channelId = 'my_channel_id';
+      const data = {
+        title: "my title",
+        message: "my message"
+      };
+      const priority = PushNotificationAndroid.PRIORITY_DEFAULT;
+      const badgeNumber = 10;
+      const showAt = 0;
+
+      PushNotificationAndroid.show(notificationId, template, channelId, data, priority, badgeNumber, showAt);
+    } catch(err) {
+      Alert.alert("Error", err);
+    }
+  }
+
+  /**
+   * Show event notification
+   */
+  onShowEventNotificationPress = () => {
+    try {
+      const notificationId = 1;
+      const template = PushNotificationAndroid.TEMPLATE_EVENT;
+      const channelId = 'my_channel_id';
+      const data = {
+        title: "title",
+        message: "my message",
+        media: "http://red-msk.ru/wp-content/uploads/2019/02/canva-photo-editor-22.png",
+        url: null
+      };
+      const priority = PushNotificationAndroid.PRIORITY_DEFAULT;
+      const badgeNumber = 10;
+      const showAt = 0;
+      PushNotificationAndroid.show(notificationId, template, channelId, data, priority, badgeNumber, showAt);
+      
+    } catch(err) {
+      Alert.alert("Error", err);
+    }
+  }
+
+  /**
+   * Show event notification with url
+   */
+  onShowEventNotificationWithUrlPress = () => {
+    try {
+      const notificationId = 1;
+      const template = PushNotificationAndroid.TEMPLATE_EVENT;
+      const channelId = 'my_channel_id';
+      const data = {
+        title: "title",
+        message: "my message",
+        media: "http://red-msk.ru/wp-content/uploads/2019/02/canva-photo-editor-22.png",
+        url: "https://google.com"
+      };
+      const priority = PushNotificationAndroid.PRIORITY_DEFAULT;
+      const badgeNumber = 10;
+      const showAt = 0;
+      PushNotificationAndroid.show(notificationId, template, channelId, data, priority, badgeNumber, showAt);
+    } catch(err) {
+      Alert.alert("Error", err);
+    }
+  }
+  
+  /**
+   * Renders JSX template
+   * @return JSX template
+   */
+  render() {
+    return (
+      <View>
+        <Text>Examples</Text>
+        
+        <View style={{marginTop: 10}} >
+          <Button title="Create channel" onPress={this.onCreateChannelPress} />
+        </View>
+
+        <View style={{marginTop: 10}} >
+          <Button title="Get device token" onPress={this.onGetDeviceTokenPress} />
+        </View>
+
+        <View style={{marginTop: 10}} >
+          <Button title="Show common notification" onPress={this.onShowCommonNotificationPress} />
+        </View>
+
+        <View style={{marginTop: 10}} >
+          <Button title="Show event notification" onPress={this.onShowEventNotificationPress} />
+        </View>
+
+        <View style={{marginTop: 10}} >
+          <Button title="Show event notification with url" onPress={this.onShowEventNotificationWithUrlPress} />
+        </View>
+      </View>
+    );
+  }
+}
